e0117777bdd562e814e53b106057a3f56864b3c0
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-10 14:13:16 -0800

    Add files via upload

---
+++ LazySource.py
@@ -0,0 +1,13 @@
+from colorama import Fore, Back, Style 
+
+
+print(Fore.BLUE + "██╗      █████╗ ███████╗██╗   ██╗███████╗ ██████╗ ██╗   ██╗██████╗  ██████╗███████╗")
+print(Fore.BLUE + "██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██╔════╝██╔═══██╗██║   ██║██╔══██╗██╔════╝██╔════╝")
+print(Fore.WHITE + "██║     ███████║  ███╔╝  ╚████╔╝ ███████╗██║   ██║██║   ██║██████╔╝██║     █████╗  ")
+print(Fore.WHITE + "██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚════██║██║   ██║██║   ██║██╔══██╗██║     ██╔══╝  ")
+print(Fore.RED + "███████╗██║  ██║███████╗   ██║   ███████║╚██████╔╝╚██████╔╝██║  ██║╚██████╗███████╗")
+print(Fore.RED + "╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚══════╝ \n\n")
+
+print(Fore.WHITE + "Loaded 28 Web Exploits\nVersion " + Fore.YELLOW + "1.2" + Fore.WHITE + "\n")
+
+text = input(Fore.WHITE + "LazySource (" + Fore.RED + "SQLExploit"+ Fore.WHITE + ") > ")
