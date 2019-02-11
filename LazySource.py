df1043714c930def8c4fe07a7b2d6b0db7bd3ab7
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-11 15:20:56 -0800

    Add files via upload

---
+++ LazySource.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+from colorama import Fore
+import time
+import git
+from git import RemoteProgress
+#import exchanges
+
+print(Fore.BLUE + "\n\n ██╗      █████╗ ███████╗██╗   ██╗███████╗ ██████╗ ██╗   ██╗██████╗  ██████╗███████╗")
+print(Fore.BLUE + " ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██╔════╝██╔═══██╗██║   ██║██╔══██╗██╔════╝██╔════╝")
+print(Fore.WHITE + " ██║     ███████║  ███╔╝  ╚████╔╝ ███████╗██║   ██║██║   ██║██████╔╝██║     █████╗  ")
+print(Fore.WHITE + " ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚════██║██║   ██║██║   ██║██╔══██╗██║     ██╔══╝  ")
+print(Fore.RED + " ███████╗██║  ██║███████╗   ██║   ███████║╚██████╔╝╚██████╔╝██║  ██║╚██████╗███████╗")
+print(Fore.RED + " ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚══════╝ \n\n")
+
+print(Fore.WHITE + "  Loaded " + Fore.GREEN + "6" + Fore.WHITE + " Tools\n  Version " + Fore.YELLOW + "1.4" + Fore.WHITE +"\n  Made By " + Fore.BLUE + "Vaxure" + Fore.WHITE + "\n<---------------------->\n")
+print(" 1) Sqlmap")
+print(" 2) JSql")
+print(" 3) BBQSQL")
+print(" 4) Nmap")
+print(" 5) Uniscan")
+print(" 6) Bitcoin Price")
+print(" 00) Update")
+
+text = raw_input(Fore.WHITE + " LazySource > ")
+if text == "00":
+    git.Repo.clone_from('https://github.com/Vaxure1337/LazySource', 'LazySource')
+elif text == "1":
+    # Sqlmap
+    print("Sqlmap")
+elif text == "2":
+    # JSql
+    print("JSql")
+if text == "3":
+    # BBQSQL
+    print("BBQSQL")
+elif text == "4":
+    # Nmap
+    print("Nmap")
+elif text == "5":
+    # Uniscan
+    print("Uniscan")
+elif text == "6":
+    print("Bitcoin")
+    #exchanges.bitfinex.get_current_price()
+else:
+    raise SystemExit
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='LazySource',
+   version='1.4',
+   description='Tool',
+   author='Vaxure',
+   author_email='vaxure@protonmail.com',
+   install_requires=['gitpython', 'colorama', 'exchanges'], #external packages as dependencies
+)
