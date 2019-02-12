ca7828ab3f58bf273b2abc0c4eb6c30874d545cf
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-12 11:38:06 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
    description='Tool',
    author='Vaxure',
    author_email='vaxure@protonmail.com',
-   install_requires=['gitpython', 'colorama', 'exchanges', 'pathlib', 'selfupdate'], #external packages as dependencies
+   install_requires=['gitpython', 'colorama', 'pathlib'], #external packages as dependencies
 )
