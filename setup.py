9b6fb9cecaa18cb8ade0adf1453d644f5502faf0
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-13 13:42:19 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
    name='LazySource',
-   version='1.6',
+   version='1.4.4',
    description='Tool',
    author='Vaxure',
    author_email='vaxure@protonmail.com',
-   install_requires=['gitpython', 'colorama', 'pathlib'], #external packages as dependencies
+   install_requires=['gitpython', 'colorama', 'pathlib', "argparse","urllib"], #external packages as dependencies
 )
