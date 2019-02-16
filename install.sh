59f68f25d67f4a8f616b95803c22106197184a1d
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-16 14:28:34 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,13 @@
 
 # A sample Bash installing script by Vaxure1337
 echo Installing LazySource
-echo -n -e  'sudo apt-get update'
-echo -n -e  'sudo apt-get upgrade'
-echo -n -e  'sudo apt-get install python'
-echo -n -e  'sudo apt-get install python-pip'
-echo -n -e  'sudo pip install pip-update'
-echo -n -e  'git clone https://github.com/Vaxure1337/LazySource'
-echo -n -e  'cd LazySource'
-echo -n -e  'sudo pip install -r requirements.txt'
-echo -n -e  'sudo python setup.py install'
-echo -n -e  'sudo python LazySource.py'
-echo -n -e  'clear'
-echo Done installing
+echo -e  'sudo apt-get update'
+echo -e  'sudo apt-get upgrade'
+echo -e  'sudo apt-get install python'
+echo -e  'sudo apt-get install python-pip'
+echo -e  'sudo pip install pip-update'
+echo -e  'git clone https://github.com/Vaxure1337/LazySource'
+echo -e  'cd LazySource'
+echo -e  'sudo pip install -r requirements.txt'
+echo -e  'sudo python setup.py install'
+echo -e  'sudo python LazySource.py'
