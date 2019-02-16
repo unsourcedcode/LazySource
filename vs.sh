8c11aece90930c4f5cda236e5f4c51de1e16266d
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-16 15:17:40 -0800

    Update vs.sh

--- vs.sh
+++ vs.sh
@@ -1,6 +1,7 @@
 #!binbash
 printf "\033c"
 echo "Made by Vaxure | LazySource Version"
+echo -e "Tool for pen-testing access your tools faster and easier for beginners.\nalso updates your pentesting tools every time you start it."
 echo -e "\n\nLazySource Last Version: "
 lynx --dump pastebin.com/raw/JN9RC4Wj > version.txt
 while read line; do echo $line; done < version.txt
