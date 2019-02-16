e0ddc194370a8dbda31c5d4fbe6beb1b85c092cd
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-16 18:53:35 -0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!binbash
+
+# A sample Bash installing script by Vaxure1337
+echo Installing LazySource
+echo 'sudo apt-get update'
+echo 'sudo apt-get upgrade'
+echo 'sudo apt-get install python'
+echo 'sudo apt-get install python-pip'
+echo 'sudo pip install pip-update'
+echo 'git clone https://github.com/Vaxure1337/LazySource'
+echo 'cd LazySource'
+echo 'sudo pip install -r requirements.txt'
+echo 'sudo python setup.py install'
+echo 'sudo python LazySource.py'
+echo 'clear'
+echo Done installing
