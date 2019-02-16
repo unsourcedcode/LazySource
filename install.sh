37656863b78360e7f2ff60f3fbb327517b0cd662
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-16 14:51:44 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!binbash
 # A sample Bash installing script by Vaxure1337
 echo "Installing LazySource"
-eval "sudo pip install -r requirements.txt"
-eval "sudo python setup.py install"
-eval "sudo python LazySource.py"
+sudo pip install -r requirements.txt
+sudo python setup.py install
+sudo python LazySource.py
