b8740d2939a5ba838761aee20e710755af6741ab
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-16 14:58:22 -0800

    Update and rename install.sh to vs.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!binbash
-# A sample Bash installing script by Vaxure1337
-echo "Installing LazySource"
-sudo pip install -r requirements.txt
-sudo python setup.py install
-sudo python LazySource.py
---
+++ vs.sh
@@ -0,0 +1,4 @@
+#!binbash
+echo "LazySource Version: "
+lynx --dump pastebin.com/raw/JN9RC4Wj > version.txt
+while read line; do echo $line; done < version.txt
