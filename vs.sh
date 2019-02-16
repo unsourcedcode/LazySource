feef60e4f10c820f228623babe939962e710fc30
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-02-16 15:13:55 -0800

    Update vs.sh

--- vs.sh
+++ vs.sh
@@ -1,6 +1,9 @@
 #!binbash
 printf "\033c"
 echo "Made by Vaxure | LazySource Version"
-echo "LazySource Last Version: "
+echo -e "\n\nLazySource Last Version: "
 lynx --dump pastebin.com/raw/JN9RC4Wj > version.txt
 while read line; do echo $line; done < version.txt
+echo -e "\n\nLazySource News: "
+lynx --dump pastebin.com/raw/fvr6QC8X > news.txt
+while read line; do echo $line; done < news.txt
